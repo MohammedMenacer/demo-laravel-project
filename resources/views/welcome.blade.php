ef20716b5f7371e6fa2009f6baba46a83602dcd5
Mohammed Menacer <mohammed1999menacer@gmail.com>
2022-05-14 17:15:56 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mohammed Menacer</title>
+</head>
+
+<body>
+    <h1>I am Mohammed Menacer</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+</body>
+
+</html>
