c3174d9c274a9d9e5104c49a66313d7381c4f49c
Mohammed Menacer <mohammed1999menacer@gmail.com>
2022-05-15 09:51:06 +0100

    A new commit to learn alias

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
     <h3>My Email Address is gm_menacer@esi.dz</h3>
     <h4>Algeria is a big country</h4>
     <h5>Mohammed Menacer is me</h5>
+    <h6>Mohammed Menacer is me</h6>
 </body>
 
 </html>
